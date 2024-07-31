d7163c5d0db99e7e06e449a973fd1df34f9be547
egallardop13 <egallardodev@gmail.com>
2024-07-30 21:42:45 -0400

    price and colors

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 function Price() {
-  return <div>Price</div>;
+  return (
+    <div className="ml">
+      <h2 className="sidebar-title price-title">Price</h2>
+      <label className="sidebar-label-container">
+        <input type="radio" className="test2" />
+        <span className="checkmark"></span>ALL
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" className="test2" />
+        <span className="checkmark"></span>$0-$50
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" className="test2" />
+        <span className="checkmark"></span>$50-$100
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" className="test2" />
+        <span className="checkmark"></span>$100-$150
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" className="test2" />
+        <span className="checkmark"></span>over $150
+      </label>
+    </div>
+  );
 }
 
 export default Price;
